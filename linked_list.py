0934bde2a60793cb65aed544ab2275550bec1bea
davekishan <75985572+davekishan@users.noreply.github.com>
2020-12-22 21:36:29 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,38 +1,43 @@
 class node:
-     def __init__(self, data):
-            self.data=data
-            self.next=none
-            
-class linkedlist:
-     def __initz__(self):
-           self.start=none
-     def insertfirst(self, value):
-           newnode = node(value)
-           if start.next == none:
-                self.start=newnode
-           else:
-                temp = self.start
-                while temp.next != none:
-                      temp=temp.next
-                temp.next = newnode
-     
-     def showlist(self):
-            if self.start == none:
-                  print("list is empty")
-            else:
-                  temp=self.start
-                  while temp.next != none:
-                        print(temp.data,end=' ')
-                        temp=temp.next
-     def deletefirst(self):
-            if self.start == none:
-                  print("list is empty")
-            else:
-                  self.start=self.start.next
-                 
-                 
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-mylist=linkedlist()
+
+class linkedlist1:
+    def __init__(self):
+        self.start = None
+
+    def insertfirst(self, value):
+        newnode = node(value)
+
+        if self.start == None:
+            self.start = newnode
+        else:
+            temp = self.start
+            while temp.next != None:
+                temp = temp.next
+            temp.next = newnode
+
+
+    def showlist(self):
+        if self.start == None:
+            print("list is empty")
+        else:
+            temp = self.start
+            while temp != None:
+                print(temp.data, end=' ')
+                temp = temp.next
+
+
+    def deletefirst(self):
+        if self.start == None:
+            print("list is empty")
+        else:
+            self.start = self.start.next
+
+
+mylist = linkedlist1()
 mylist.insertfirst(10)
 mylist.insertfirst(20)
 mylist.insertfirst(30)
@@ -41,5 +46,3 @@ mylist.showlist()
 print()
 mylist.deletefirst()
 mylist.showlist()
-
-             
