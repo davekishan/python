54afdaa437f8f78f799831f435592619ca008e17
davekishan <75985572+davekishan@users.noreply.github.com>
2020-12-23 00:11:51 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -8,7 +8,7 @@ class linkedlist1:
     def __init__(self):
         self.start = None
 
-    def insertfirst(self, value):
+    def insertlast(self, value):
         newnode = node(value)
 
         if self.start == None:
@@ -18,6 +18,7 @@ class linkedlist1:
             while temp.next != None:
                 temp = temp.next
             temp.next = newnode
+        print("inserting last", value)
 
 
     def showlist(self):
@@ -29,20 +30,49 @@ class linkedlist1:
                 print(temp.data, end=' ')
                 temp = temp.next
 
-
     def deletefirst(self):
         if self.start == None:
             print("list is empty")
         else:
+            node = self.start.data
             self.start = self.start.next
+        print("deleting first node", node)
+
+
+    def insertfirst(self, value):
+        newnode = node(value)
+        if self.start == None:
+            self.start=newnode
+        else:
+            newnode.next = self.start
+            self.start = newnode
+        print("insert first", value)
+
+    def deletelast(self):
+        if self.start == None:
+            print("list is empty")
+        elif self.start.next == None:
+            self.start = None
+        else:
+            temp = self.start
+            while temp.next.next != None:
+                temp = temp.next
+            temp.next = None
+        print("deleting last node")
 
 
 mylist = linkedlist1()
-mylist.insertfirst(10)
-mylist.insertfirst(20)
-mylist.insertfirst(30)
-mylist.insertfirst(40)
+mylist.insertlast(10)
+mylist.insertlast(20)
+mylist.insertlast(30)
+mylist.insertlast(40)
+mylist.showlist()
+print()
+mylist.insertfirst(50)
 mylist.showlist()
 print()
 mylist.deletefirst()
+mylist.showlist()
+print()
+mylist.deletelast()
 mylist.showlist()
